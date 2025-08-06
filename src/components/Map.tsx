97947c20176ed83ad7f336837fee3239876ab0a5
faishalshidqi <rshidqi6@gmail.com>
2025-08-06 11:35:18 +0700

    added geolocation fetching function

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,15 +1,31 @@
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
-import type {LatLngExpression} from "leaflet";
+import {useState} from "react";
 
 export default function Map() {
-    const pos = [51.505, -0.09] as LatLngExpression;
+    const [latitude, setLatitude] = useState(0)
+    const [longitude, setLongitude] = useState(0)
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+            (position: GeolocationPosition) => {
+                setLatitude(position.coords.latitude)
+                setLongitude(position.coords.longitude)
+            },
+            (position) => {
+                console.error(position)
+            }
+        );
+    }
+    else {
+        alert("Geolocation is not supported. Allow access to geolocation!");
+    }
+    console.log(latitude, longitude)
     return (
-        <MapContainer center={pos} zoom={13} scrollWheelZoom={false}>
+        <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={pos}>
+            <Marker position={[latitude, longitude]}>
                 <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                 </Popup>
