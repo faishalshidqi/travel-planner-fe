b26794d157390f0fadb9b4845eb3a3dd6a5dacfb
faishalshidqi <rshidqi6@gmail.com>
2025-08-18 14:05:53 +0700

    added dashboard page

--- src/components/nav-documents.tsx
+++ src/components/nav-documents.tsx
@@ -22,8 +22,8 @@ import {
   SidebarMenuAction,
   SidebarMenuButton,
   SidebarMenuItem,
-  useSidebar,
 } from "@/components/ui/sidebar"
+import {useSidebar} from "@/components/ui/imports/useSidebar.ts";
 
 export function NavDocuments({
   items,
